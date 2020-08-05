531b7c8af480975bea44f1c140de4d916a2c1c88
mazj <silas_ma@163.com>
2020-08-05 19:09:17 +0800

    夜间模式

--- app/src/main/java/pot/ner347/qskin/MainActivity.kt
+++ app/src/main/java/pot/ner347/qskin/MainActivity.kt
@@ -3,9 +3,11 @@ package pot.ner347.qskin
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import pot.ner347.qskin.skin.SkinBean
@@ -47,6 +49,39 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this@MainActivity, SecondActivity::class.java))
         }
 
+        findViewById<Button>(R.id.night).setOnClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            recreate()
+        }
+        findViewById<Button>(R.id.day).setOnClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            recreate()
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        val mode = newConfig.uiMode and Configuration.UI_MODE_NIGHT_MASK
+
+        when(mode) {
+            Configuration.UI_MODE_NIGHT_NO -> {}
+            Configuration.UI_MODE_NIGHT_YES -> {}
+        }
+    }
+
+    override fun onNightModeChanged(mode: Int) {
+        super.onNightModeChanged(mode)
+        when (mode) {
+            AppCompatDelegate.MODE_NIGHT_NO -> {
+            }
+            AppCompatDelegate.MODE_NIGHT_YES -> {
+            }
+            AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY -> {
+            }
+            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM -> {
+            }
+        }
     }
 
     private fun selectSkin(skin: SkinBean) {
